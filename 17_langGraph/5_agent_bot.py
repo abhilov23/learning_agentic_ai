d0a875cb42697b5bebe81d52eb5fff23bcf6912e
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2026-05-02 08:20:56 +0530

    build first langgraph model

---
+++ 17_langGraph/5_agent_bot.py
@@ -0,0 +1,34 @@
+from typing import TypedDict, List 
+from langchain_core.messages import HumanMessage
+from langchain_nvidia import ChatNVIDIA
+from langgraph.graph import StateGraph, START, END 
+from dotenv import load_dotenv 
+import os
+load_dotenv()
+
+#model="meta/llama-3.1-70b-instruct",
+#NVIDIA_API_KEY
+
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+
+llm  = ChatNVIDIA(model="meta/llama-3.1-70b-instruct", api_key=os.getenv("NVIDIA_API_KEY"),)
+
+
+def process(state:AgentState)-> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"Response: {response.content}")
+    return state 
+
+
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+user_input = input("Enter your message: ")
+agent.invoke({"messages": [HumanMessage(content=user_input)]})
