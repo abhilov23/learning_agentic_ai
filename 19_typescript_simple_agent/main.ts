8af9ea651fc37f3f1491e48adb1b1e5ba2f387a4
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2026-05-09 00:00:16 +0530

    first agent in the typeScript

--- 19_typescript_simple_agent/main.ts
+++ 19_typescript_simple_agent/main.ts
@@ -1,12 +1,58 @@
-import { createDeepAgent } from "deepagents";
+import "dotenv/config";
+import { ChatOpenAI } from "@langchain/openai";
+import {z} from "zod";
+import { createAgent, tool } from "langchain";
 
-const agent = createDeepAgent();
 
+
+// defining the tool
+const search = tool(
+    async ({query}) => {
+     if(
+      query.toLowerCase().includes("sf") ||
+      query.toLowerCase().includes("san francisco")
+     ) {
+        return "It's 60 degree and foggy."
+     } 
+     return "it's 90 degree and sunny"
+    },
+    {
+        name: "Search",
+        description: "Search for the current weather in San Francisco",
+        schema: z.object({
+            query: z.string().describe("The query to use in your search.")
+        })
+    }
+);
+
+
+// connecting with the api 
+const model = new ChatOpenAI({
+  model: "meta/llama-3.1-70b-instruct",
+  apiKey: process.env.NVIDIA_API_KEY,
+  configuration: {
+    baseURL: "https://integrate.api.nvidia.com/v1",
+  },
+});
+
+
+
+// creating the ReAct Agent
+const agent = createAgent({
+   model, 
+   tools: [search]
+});
+
+
+// invoking the agent
 const result = await agent.invoke({
   messages: [
     {
       role: "user",
-      content: "Research LangGraph and write a summary in summary.md",
-    },
-  ],
+      content: "what is the weather in San Francisco?"
+    }
+  ]
 });
+
+
+console.log(result);
